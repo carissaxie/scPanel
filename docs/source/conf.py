4a2507c1b1141d867c52289c53dcf134d46d9747
carissaxie <yixie0822@outlook.com>
2024-10-16 21:12:14 +0700

    update doc configure file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'scPanel'
 copyright = '2024, Yi Carissa Xie'
 author = 'Yi Carissa Xie'
-release = '0.1.0'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
